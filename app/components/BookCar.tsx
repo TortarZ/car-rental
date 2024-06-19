777fd3805a90fa65e4f6c06ecd7019ef830a7607
TortarZ <tortarz1999@gmail.com>
2024-06-19 14:10:43 +0700

    Make Bookcar section

---
+++ app/components/BookCar.tsx
@@ -0,0 +1,159 @@
+import React from "react";
+import "./Styles/BookCar.css";
+
+const BookCar = () => {
+  return (
+    <>
+      <section className="book-section">
+        <div className="bookcar-con">
+          <h2>Book a car</h2>
+          <form className="box-form">
+            <div className="bookcar-content">
+              <div className="selectcar-con">
+                <label>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="input-icon"
+                  >
+                    <path d="M7 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+                    <path d="M17 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+                    <path d="M5 17h-2v-6l2 -5h9l4 5h1a2 2 0 0 1 2 2v4h-2m-4 0h-6m-6 -6h15m-6 0v-5"></path>
+                  </svg>
+                  Select Your Car Type <b>*</b>
+                </label>
+                <select>
+                  <option>Select your car type</option>
+                  <option value="Audi A1 S-Line">Audi A1 S-Line</option>
+                  <option value="VW Golf 6">VW Golf 6</option>
+                  <option value="Toyota Camry">Toyota Camry</option>
+                  <option value="BMW 320 ModernLine">BMW 320 ModernLine</option>
+                  <option value="Mercedes-Benz GLK">Mercedes-Benz GLK</option>
+                  <option value="VW Passat CC">VW Passat CC</option>
+                </select>
+              </div>
+              <div className="picklocation-con">
+                <label>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="input-icon"
+                  >
+                    <path d="M7 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+                    <path d="M17 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+                    <path d="M5 17h-2v-6l2 -5h9l4 5h1a2 2 0 0 1 2 2v4h-2m-4 0h-6m-6 -6h15m-6 0v-5"></path>
+                  </svg>
+                  Select Your Car Type <b>*</b>
+                </label>
+                <select>
+                  <option>Select your car type</option>
+                  <option value="Audi A1 S-Line">Audi A1 S-Line</option>
+                  <option value="VW Golf 6">VW Golf 6</option>
+                  <option value="Toyota Camry">Toyota Camry</option>
+                  <option value="BMW 320 ModernLine">BMW 320 ModernLine</option>
+                  <option value="Mercedes-Benz GLK">Mercedes-Benz GLK</option>
+                  <option value="VW Passat CC">VW Passat CC</option>
+                </select>
+              </div>
+              <div className="dropof-con">
+                <label>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="input-icon"
+                  >
+                    <path d="M7 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+                    <path d="M17 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+                    <path d="M5 17h-2v-6l2 -5h9l4 5h1a2 2 0 0 1 2 2v4h-2m-4 0h-6m-6 -6h15m-6 0v-5"></path>
+                  </svg>
+                  Select Your Car Type <b>*</b>
+                </label>
+                <select>
+                  <option>Select your car type</option>
+                  <option value="Audi A1 S-Line">Audi A1 S-Line</option>
+                  <option value="VW Golf 6">VW Golf 6</option>
+                  <option value="Toyota Camry">Toyota Camry</option>
+                  <option value="BMW 320 ModernLine">BMW 320 ModernLine</option>
+                  <option value="Mercedes-Benz GLK">Mercedes-Benz GLK</option>
+                  <option value="VW Passat CC">VW Passat CC</option>
+                </select>
+              </div>
+              <div className="pickup-con">
+                <label>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="input-icon"
+                  >
+                    <path d="M7 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+                    <path d="M17 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+                    <path d="M5 17h-2v-6l2 -5h9l4 5h1a2 2 0 0 1 2 2v4h-2m-4 0h-6m-6 -6h15m-6 0v-5"></path>
+                  </svg>
+                  Pick-up <b>*</b>
+                </label>
+                <input type="date" name="pick-up" id="" />
+              </div>
+              <div className="dropof-con">
+                <label>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="input-icon"
+                  >
+                    <path d="M7 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+                    <path d="M17 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+                    <path d="M5 17h-2v-6l2 -5h9l4 5h1a2 2 0 0 1 2 2v4h-2m-4 0h-6m-6 -6h15m-6 0v-5"></path>
+                  </svg>
+                  Drop-of <b>*</b>
+                </label>
+                <input type="date" name="drop-of" id="" />
+              </div>
+              <div className="submit-con">
+                <button type="submit" className="submit-btn button">
+                  Search
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default BookCar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import BookCar from "./components/BookCar";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <BookCar />
     </>
   );
 }

581f63cecb877e39dd0c6309e57b6033ec96a65d
TortarZ <tortarz1999@gmail.com>
2024-07-02 21:27:45 +0700

    feat (FAQ): Make interactive menu for display answer

--- app/Home/FAQ.tsx
+++ app/Home/FAQ.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+"use client";
+import { useState } from "react";
 import "./Styles/FAQ.css";
 
+const faqData = [
+  {
+    question: "What is special about comparing rental car deals",
+    answer:
+      "Comparing rental car deals is important as it helps find the best deal that fits your budget and requirements, ensuring you get the most value for your money. By comparing various options, you can find deals that offer lower prices, additional services, or better car models. You can find car rental deals by researching online and comparing prices from different rental companies.",
+  },
+  {
+    question: "How do I find the car rental deals",
+    answer:
+      "You can find car rental deals by researching online and comparing prices from different rental companies. Websites such as Expedia, Kayak, and Travel city allow you to compare prices and view available rental options. It is also recommended to sign up for email newsletters and follow rental car companies on social media to be informed of any special deals or promotions.",
+  },
+  {
+    question: "How do I find such low rental car prices",
+    answer:
+      "Book in advance: Booking your rental car ahead of time can often result in lower prices. Compare prices from multiple companies: Use websites like Kayak, Expedia, or Travel city to compare prices from multiple rental car companies. Look for discount codes and coupons: Search for discount codes and coupons that you can use to lower the rental price. Renting from an off-airport location can sometimes result in lower prices.",
+  },
+];
+
 const FAQ = () => {
+  const [selectQuestion, setSelectQuestion] = useState(faqData[0]);
+
+  const handleClick = (faq: any) => {
+    setSelectQuestion(faq);
+  };
   return (
     <>
       <section className="faq-con">
@@ -14,71 +38,45 @@ const FAQ = () => {
           </p>
         </div>
         <div className="faq-con-content">
-          <div className="faq-box">
-            <div className="question active-question">
-              <p>1. What is special about comparing rental car deals?</p>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="tabler-icon tabler-icon-chevron-down"
+          {faqData.map((faq, i) => (
+            <div className="faq-box">
+              <a
+                onClick={() => handleClick(faq)}
+                className={`question ${
+                  selectQuestion.question === faq.question
+                    ? "active-question"
+                    : ""
+                }`}
               >
-                <path d="M6 9l6 6l6 -6"></path>
-              </svg>
-            </div>
-            <div className="active-answer">
-              Comparing rental car deals is important as it helps find the best
-              deal that fits your budget and requirements, ensuring you get the
-              most value for your money. By comparing various options, you can
-              find deals that offer lower prices, additional services, or better
-              car models. You can find car rental deals by researching online
-              and comparing prices from different rental companies.
-            </div>
-          </div>
-          <div className="faq-box">
-            <div className="question">
-              <p>2. How do I find the car rental deals?</p>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="tabler-icon tabler-icon-chevron-down"
+                <p>
+                  {i + 1}. {faq.question}?
+                </p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="tabler-icon tabler-icon-chevron-down"
+                >
+                  <path d="M6 9l6 6l6 -6"></path>
+                </svg>
+              </a>
+              <div
+                className={`answer ${
+                  selectQuestion.question === faq.question
+                    ? "active-answer"
+                    : ""
+                }`}
               >
-                <path d="M6 9l6 6l6 -6"></path>
-              </svg>
+                {faq.answer}
+              </div>
             </div>
-          </div>
-          <div className="faq-box">
-            <div className="question">
-              <p>3. How do I find such low rental car prices?</p>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="tabler-icon tabler-icon-chevron-down"
-              >
-                <path d="M6 9l6 6l6 -6"></path>
-              </svg>
-            </div>
-          </div>
+          ))}
         </div>
       </section>
     </>

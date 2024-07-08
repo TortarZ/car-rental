c87bd3ef066ce76053a7550d50d81a17c3ec28ad
TortarZ <tortarz1999@gmail.com>
2024-07-08 14:06:41 +0700

    feat (FAQ): interactive

--- app/Home/FAQ.tsx
+++ app/Home/FAQ.tsx
@@ -4,16 +4,19 @@ import "./Styles/FAQ.css";
 
 const faqData = [
   {
+    id: 1,
     question: "What is special about comparing rental car deals",
     answer:
       "Comparing rental car deals is important as it helps find the best deal that fits your budget and requirements, ensuring you get the most value for your money. By comparing various options, you can find deals that offer lower prices, additional services, or better car models. You can find car rental deals by researching online and comparing prices from different rental companies.",
   },
   {
+    id: 2,
     question: "How do I find the car rental deals",
     answer:
       "You can find car rental deals by researching online and comparing prices from different rental companies. Websites such as Expedia, Kayak, and Travel city allow you to compare prices and view available rental options. It is also recommended to sign up for email newsletters and follow rental car companies on social media to be informed of any special deals or promotions.",
   },
   {
+    id: 3,
     question: "How do I find such low rental car prices",
     answer:
       "Book in advance: Booking your rental car ahead of time can often result in lower prices. Compare prices from multiple companies: Use websites like Kayak, Expedia, or Travel city to compare prices from multiple rental car companies. Look for discount codes and coupons: Search for discount codes and coupons that you can use to lower the rental price. Renting from an off-airport location can sometimes result in lower prices.",
@@ -23,8 +26,8 @@ const faqData = [
 const FAQ = () => {
   const [selectQuestion, setSelectQuestion] = useState(faqData[0]);
 
-  const handleClick = (faq: any) => {
-    setSelectQuestion(faq);
+  const toggle = (i: any) => {
+    setSelectQuestion(i);
   };
   return (
     <>
@@ -37,47 +40,28 @@ const FAQ = () => {
             Our Website: Answers to Common Concerns and Inquiries.
           </p>
         </div>
-        <div className="faq-con-content">
-          {faqData.map((faq, i) => (
-            <div className="faq-box">
-              <a
-                onClick={() => handleClick(faq)}
-                className={`question ${
-                  selectQuestion.question === faq.question
-                    ? "active-question"
-                    : ""
-                }`}
-              >
-                <p>
-                  {i + 1}. {faq.question}?
-                </p>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="tabler-icon tabler-icon-chevron-down"
-                >
-                  <path d="M6 9l6 6l6 -6"></path>
-                </svg>
-              </a>
-              <div
-                className={`answer ${
-                  selectQuestion.question === faq.question
-                    ? "active-answer"
-                    : ""
-                }`}
-              >
-                {faq.answer}
-              </div>
+        {faqData.map((item, i) => (
+          <div className="faq-con-content" key={item.id}>
+            <div
+              className={`question ${
+                selectQuestion.question === item.question ? "active" : ""
+              }`}
+              onClick={() => toggle(item)}
+            >
+              <p>
+                {i + 1}.{item.question}
+              </p>
+              <span>&#709;</span>
             </div>
-          ))}
-        </div>
+            <div
+              className={`answer ${
+                selectQuestion.answer === item.answer ? "show" : ""
+              }`}
+            >
+              <p>{item.answer}</p>
+            </div>
+          </div>
+        ))}
       </section>
     </>
   );

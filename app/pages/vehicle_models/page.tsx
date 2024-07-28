f258267c918a26d24d8d271b0322162e85b4ff8a
TortarZ <tortarz1999@gmail.com>
2024-07-28 13:12:19 +0700

    style (Header): change css that is managed for responsive support

--- app/pages/vehicle_models/page.tsx
+++ app/pages/vehicle_models/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const VehicleModels = () => {
+  return <div>vehicle models</div>;
 };
 
-export default page;
+export default VehicleModels;
